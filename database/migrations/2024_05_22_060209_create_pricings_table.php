4afacef98525cd24360bf94be77e6703a565cdc5
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-29 01:56:54 +0600

    Migration for previous pricing, message, orders

---
+++ database/migrations/2024_05_22_060209_create_pricings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pricings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->integer('price');
+            $table->string('job1');
+            $table->string('job2');
+            $table->string('job3');
+            $table->string('job4');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pricings');
+    }
+};
