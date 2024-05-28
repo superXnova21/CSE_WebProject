2b37a4426a40083de0c8de7cb33c1d35fc8d0ecc
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-29 02:02:42 +0600

    Previous events and about section done.

---
+++ resources/views/prevevents/create.blade.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Create</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/create_style.css')}}">
+</head>
+
+<body>
+    <div class="container register">
+
+        <div class="row">
+
+            
+            <div class="col-md-9 register-right">
+
+                <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+                
+                    
+                </ul>
+
+                <div class="tab-content" id="myTabContent">
+
+                    <div class="tab-pane fade show active" id="club" role="tabpanel" aria-labelledby="profile-tab">
+                        <h3 class="register-heading">Previous Events</h3>
+                        <form action="/admin/prev-events/store" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row register-form">
+
+                                
+                                    
+                                    <div class="form-group">
+                                        <input type="file" name="image1" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" name="image2" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" name="image3" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" name="image4" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" name="image5" required="">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="file" name="image6" required="">
+                                    </div>
+                                    {{-- <div class="form-group">
+                                        <input type="text" name="founded_year" class="form-control"
+                                            placeholder="Founded Year" value="" />
+                                    </div> --}}
+
+                                    <button type="submit">
+                                        <span>
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                                                height="24">
+                                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                                <path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path>
+                                            </svg> Create
+                                        </span>
+                                    </button>
+
+                                
+
+                            </div>
+                        </form>
+
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomePageController;
 
-
+Route::controller(HomePageController::class)->group(function () {
+    Route::get('/', 'index')->name('home.index');
+    Route::get('/admin', 'admin')->name('home.admin');
+    // Message
+    Route::post('/message/store','storeMessage')->name('message.store');
+});
