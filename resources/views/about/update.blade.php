e4aa2d6eba6a359cec5b1d86a39d8f1de8a3ed4d
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-29 02:22:39 +0600

    Update view for about section

---
+++ resources/views/about/update.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Create</title>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/create_style.css') }}">
+    <style>
+        #form{
+            display: flex;
+            flex-direction: column;
+            gap: 1rem;
+        }
+        .error-message{
+            color: red;
+        }
+        .form-group input{
+            width: 340px
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container register">
+
+        <div class="row">
+
+
+            <div class="col-md-9 register-right">
+
+                <ul class="nav nav-tabs nav-justified" id="myTab" role="tablist">
+
+
+                </ul>
+
+                <div class="tab-content" id="myTabContent">
+
+                    <div class="tab-pane fade show active" id="club" role="tabpanel" aria-labelledby="profile-tab">
+                        <h3 class="register-heading">About Us</h3>
+                        <form  action="/admin/about-us/update/{{$about->id}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row register-form" id="form">
+
+                                <div class="form-group">
+                                    <input type="text" name="title" class="form-control" placeholder="Title"
+                                        value="{{ old('title', $about->title) }}" required="" />
+                                    @error('title')
+                                        <div class="error-message">
+                                            <p>{{ $message }}</p>
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="file" name="image" class="form-control" placeholder="Image"
+                                        value="{{ old('image') }}"  />
+                                    @error('image')
+                                        <div class="error-message">
+                                            <p>{{ $message }}</p>
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <textarea name="description" id="" required="" cols="50" rows="10" placeholder="Description">
+                                        {{ old('description',$about->description) }}
+                                    </textarea>
+                                    @error('description')
+                                        <div class="error-message">
+                                            <p>{{ $message }}</p>
+                                        </div>
+                                    @enderror
+                                </div>
+
+                                
+
+                                <button type="submit">
+                                    <span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                                            height="24">
+                                            <path fill="none" d="M0 0h24v24H0z"></path>
+                                            <path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path>
+                                        </svg> Update
+                                    </span>
+                                </button>
+
+
+
+                            </div>
+                        </form>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -28,36 +28,3 @@ Route::controller(AuthController::class)->group(function () {
 
 
 
-// Admin Routes List
-Route::middleware(['auth', 'user-access:admin'])->group(function () {
-    Route::controller(HomePageController::class)->group(function () {
-        Route::get('/admin', 'admin')->name('home.admin');
-        // Previous Events
-        Route::get('/admin/prev-events', 'prevEvents')->name('prevevents.create');
-        Route::post('/admin/prev-events/store', 'storeEvents')->name('prevevents.store');
-        Route::get('/admin/prev-events/edit/{id}', 'editEvents')->name('prevevents.update');
-        Route::post('/admin/prev-events/update/{id}', 'updateEvents')->name('prevevents.update');
-        Route::delete('/admin/prev-events/{id}','destroy')->name('prev-events.destroy');
-        // Services
-        Route::get('/admin/services', 'services')->name('service.create');
-        Route::post('/admin/services/store', 'storeService')->name('service.store');
-        Route::get('/admin/services/edit/{id}', 'editService')->name('service.update');
-        Route::post('/admin/services/update/{id}', 'updateService')->name('service.update');
-        Route::delete('/admin/services/{id}','destroyService')->name('service.destroy');
-        // About us
-        Route::get('/admin/about-us', 'aboutUs')->name('about.create');
-        Route::post('/admin/about-us/store', 'storeAbout')->name('about.store');
-        Route::get('/admin/about-us/edit/{id}', 'editAbout')->name('about.update');
-        Route::post('/admin/about-us/update/{id}', 'updateAbout')->name('about.update');
-        Route::delete('/admin/about-us/{id}','destroyAbout')->name('about.destroy');
-        // Pricing
-        Route::get('/admin/pricing', 'pricing')->name('pricing.create');
-        Route::post('/admin/pricing/store', 'storePricing')->name('pricing.store');
-        Route::get('/admin/pricing/edit/{id}', 'editPricing')->name('pricing.update');
-        Route::post('/admin/pricing/update/{id}', 'updatePricing')->name('pricing.update');
-        Route::delete('/admin/pricing/{id}','destroyPricing')->name('pricing.destroy');
-        // Delete message
-        Route::delete('/admin/message/{id}','destroyMessage')->name('message.destroy');
-
-    });
-});
