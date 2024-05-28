31790de30868a90deb55083011ff081e7bab6aae
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-05-29 01:54:06 +0600

    Migration for user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('type')->default(false); // add type boolean Users=0, Admin=1
             $table->rememberToken();
             $table->timestamps();
         });
